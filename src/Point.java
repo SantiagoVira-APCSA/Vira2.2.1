83af6e1872777fb4867d378341662d9069253620
SantiagoVira <santiagovira06@gmail.com>
2022-10-11 12:32:48 -0400

    Code

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+    private int x, y;
+
+    // Individual x and y
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Set x and y to same thing
+    public Point(int value) {
+        x = value;
+        y = value;
+    }
+
+    // Set x and y to 0 (origin)
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int newX) {
+        x = newX;
+    }
+
+    public void setY(int newY) {
+        y = newY;
+    }
+
+    public String coordinate() {
+        return "(" + x + "," + y + ")";
+    }
+
+    public String quadrant() {
+        if (x == 0 && y == 0) {
+            return "On the origin";
+        }
+        if (x == 0 || y == 0) {
+            return "On an axis";
+        }
+        if (y > 0) {
+            return x > 0 ? "I" : "II";
+        } else {
+            return x < 0 ? "III" : "IV";
+        }
+    }
 }
--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class RectangleRunner {
+public class RectangleRunner {
+    public static void main(String[] args) {
+        // Instantiate rectangles
+        Rectangle plot1 = new Rectangle(150, 200);
+        Rectangle plot2 = new Rectangle(125);
+        Rectangle plot3 = new Rectangle();
+
+        // Calculate total area
+        int totalArea = plot1.calculateArea() + plot2.calculateArea() + plot3.calculateArea();
+        System.out.println("These three plots require " + totalArea + " square feet of seed");
+
+        // Update all plot widths to 75
+        plot1.setWidth(75);
+        plot2.setWidth(75);
+        plot2.setLength(75);
+        plot3.setWidth(75);
+
+        // Calculate total area
+        totalArea = plot1.calculateArea() + plot2.calculateArea() + plot3.calculateArea();
+        System.out.println("These three plots require " + totalArea + " square feet of seed");
+
+//        int len = 65;
+//        int wid = len + 10;
+//        Rectangle rect1 = new Rectangle(len, wid);
+//        Rectangle rect2 = new Rectangle(10.0, 30.0);
+//        Rectangle rect3 = new Rectangle(0);
+
+    }
 }
